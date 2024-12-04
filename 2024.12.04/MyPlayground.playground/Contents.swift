c7eb1611967a1878208137f6184a2753c44580ea
tomchoi95 <qjatn0545@naver.com>
2024-12-04 21:09:16 +0900

    기초

---
+++ 2024.12.04/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit // 이걸 Frame Work라고 함
+
+var greeting = "Hello, playground"
+
+let starImage = UIImage(systemName: "star") //playground에서 보면 좀 자세하게 볼 수 있음.
+
+var sum = 0
+for i in 1 ... 10 {
+    sum += i
+}
+
+print(sum)
+
+dump(greeting)
+/*
+여기에
+있다면
+주석이
+된다네
+ */
+
--- 2024.12.04/README.md
+++ 2024.12.04/README.md
@@ -1,1 +1,1 @@
-# DevNote
+
