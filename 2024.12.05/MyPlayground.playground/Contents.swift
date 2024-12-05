e652ab7245346e543474b1aaed8aa1ec5b5740d8
tomchoi95 <qjatn0545@naver.com>
2024-12-05 10:01:15 +0900

    UI

--- 2024.12.05/MyPlayground.playground/Contents.swift
+++ 2024.12.05/MyPlayground.playground/Contents.swift
@@ -45,3 +45,38 @@ let lat: Coordinate = 12.34
 let lon: Coordinate = 45.67
 
 
+/* UI
+ 텍스트 필드는 한줄 입력.
+ 텍스트필드에 텍스트를 쓰면 텍스트가 바뀜
+ 폰트 설정 가능.
+ 얼라인먼트(정렬) 가능
+ 
+ 버튼.
+ 스타일 plain -> default
+ 타이틀 // 버튼에 나오는 글씨
+ 
+ UIView //가장 기본적은 뷰
+ 컨테이너.
+ 
+ Lable
+ 
+ 오토레이아웃. 제약(constraint)
+ */
+
+// ViewContriller
+
+class ViewController: UIViewController {
+    @IBOutlet weak var firstOperandField: UITextField!
+    @IBOutlet weak var operatorButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func selectOperator(_ sender: Any) {
+        
+    }
+    @IBAction func calculate(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
